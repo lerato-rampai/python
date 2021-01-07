b0ff7adef61664baf45f78768f494336900bc1e8
Lerato Rampai <lerato.rampail@gmail.com>
2021-01-07 18:17:42 +0200

    Update variables.py

--- the_basics/variables.py
+++ the_basics/variables.py
@@ -9,10 +9,27 @@ text = "Lerato"
 print(text, number)
 
 #simple date types integers, floats and strings
-
+rank = 10
+eggs = 12
+people = 3
 x = 20
+
+print("Integers are for representing whole numbers:")
+print(rank, eggs, people)
+
 y = "love"
+message = "Welcome to our online shop!"
+name = "John"
+serial = "R001991981SW"
+print("Strings represent any text:")
+print(y, message, name, serial)
+
+temperature = 10.2
+rainfall = 5.98
+elevation = 1031.88
 z = 20.5
+print("Floats represent continuous values:")
+print(temperature, rainfall, elevation, z)
 
 sum1 = x + x
 sum2 = y + y
