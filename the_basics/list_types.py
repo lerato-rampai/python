d3c5efca96d55961bee3156f9111a3e01781dedb
Lerato Rampai <lerato.rampail@gmail.com>
2021-01-07 18:17:31 +0200

    Update list_types.py

--- the_basics/list_types.py
+++ the_basics/list_types.py
@@ -1,4 +1,8 @@
+members = ["Sim Soony", "Marry Roundknee", "Jack Corridor"]
+pixel_values = [252, 251, 251, 253, 250, 248, 247]
 student_grades = [59, 70, 90, 80, 49, 99]
+print("Lists represent arrays of values that may change during the course of the program:")
+print(members, pixel_values, student_grades)
 
 
 #creating a range
@@ -22,6 +26,16 @@ student_grades = {"Mpho": 59, "Dineo": 70, "Letlotlo": 90, "Dimpho": 80, "Teboho
 grades_sum = sum(student_grades.values())
 grades_sum = sum(student_grades.values())
 length = len(student_grades)
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+volcano_elevations = {"Glacier Peak": 3213.9, "Rainer": 4392.1}
+print("Dictionaries represent pairs of keys and values:")
+print(phone_numbers, volcano_elevations)
+print("")
+print("Keys of a dictionary can be extracted with:")
+print(phone_numbers.keys())
+
+print("Values of a dictionary can be extracted with:")
+print(phone_numbers.values())
 
 #calculating the mean
 mean = grades_sum / length
@@ -29,3 +43,12 @@ print(mean)
 
 print(grades_sum)
 print(student_grades.keys())
+
+#tuple list types
+mon_temperatures = (-1, 5, 9)
+print(mon_temperatures)
+print("Tuples represent arrays of values that are not to be changed during the course of the program:")
+
+vowels = ('a', 'e', 'i', 'o', 'u')
+one_digits = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+print(vowels, one_digits)
