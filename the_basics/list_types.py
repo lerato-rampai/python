fc852c37119abf4b747eaa08ef74f73f86c49988
Lerato Rampai <lerato.rampail@gmail.com>
2021-01-07 18:02:47 +0200

    Added dictionaries

--- the_basics/list_types.py
+++ the_basics/list_types.py
@@ -1,5 +1,6 @@
 student_grades = [59, 70, 90, 80, 49, 99]
 
+
 #creating a range
 list(range(2, 10))
 
@@ -13,3 +14,18 @@ length = len(student_grades)
 #calculating the mean
 mean = grades_sum / length
 print(mean)
+
+#dictionaries
+temperatures = [29.8, 23.5, 34.5, 30.5]
+student_grades = {"Mpho": 59, "Dineo": 70, "Letlotlo": 90, "Dimpho": 80, "Teboho": 49, "Lerato": 99}
+
+grades_sum = sum(student_grades.values())
+grades_sum = sum(student_grades.values())
+length = len(student_grades)
+
+#calculating the mean
+mean = grades_sum / length
+print(mean)
+
+print(grades_sum)
+print(student_grades.keys())
