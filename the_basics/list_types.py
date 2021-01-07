63d1bf5e4b81b131c7d78b7d323ff3efd62c99cf
Lerato Rampai <lerato.rampail@gmail.com>
2021-01-07 17:21:53 +0200

    Calculted mean

--- the_basics/list_types.py
+++ the_basics/list_types.py
@@ -4,5 +4,12 @@ student_grades = [59, 70, 90, 80, 49, 99]
 list(range(2, 10))
 
 #You can also specify a step as a third argument:
-
 list(range(1, 10, 2))
+
+#calculating the sum of the grades
+grades_sum = sum(student_grades)
+length = len(student_grades)
+
+#calculating the mean
+mean = grades_sum / length
+print(mean)
