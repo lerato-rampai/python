3e78923d88d0676055fd86fd683fd120a6767adc
Lerato Rampai <lerato.rampail@gmail.com>
2021-01-07 16:54:34 +0200

    Added types of variables

--- the_basics/variables.py
+++ the_basics/variables.py
@@ -7,3 +7,15 @@ number = 27
 text = "Lerato"
 
 print(text, number)
+
+#simple date types integers, floats and strings
+
+x = 20
+y = "love"
+z = 20.5
+
+sum1 = x + x
+sum2 = y + y
+
+print(sum1, sum2)
+print(type(x), type(y), type(z))
